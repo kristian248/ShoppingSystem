ee55d2c4ec24ee314d1a258ea540099a4dbfb1e8
kristian248 <PC-140-3@DESKTOP-L7IFOBR>
2022-06-02 11:41:43 +0300

    rabota

---
+++ ShoppingSystem/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShoppingSystem;
+
+namespace ShoppingSystem
+{
+    public class Controller
+    {
+        public Controller()
+        {
+        }
+        public string ProcessProductCommand(List<string> args)
+        {
+            // TODO: Implement me...
+            return null;
+        }
+        public string ProcessServiceCommand(List<string> args)
+        {
+            // TODO: Implement me...
+            return null;
+        }
+        public string ProcessCheckoutCommand(List<string> args)
+        {
+            // TODO: Implement me...
+            return null;
+        }
+        public string ProcessInfoCommand(List<string> args)
+        {
+            // TODO: Implement me...
+            return null;
+        }
+        public string ProcessEndCommand()
+        {
+            // TODO: Implement me...
+            return null;
+        }
+
+    }
+}
---
+++ ShoppingSystem/Engine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSystem
+{
+    public class Engine
+    {
+        private Controller controller;
+        private bool isRunning;
+        public Engine(Controller controller)
+        {
+            this.controller = controller;
+            this.isRunning = true;
+        }
+        public void Run()
+        {
+            while (isRunning)
+            {
+                string[] splittedInput = Console.ReadLine().Split();
+                string command = splittedInput[0];
+                List<string> arguments = splittedInput
+                    .Skip(1)
+                    .ToList();
+                string result = "";
+                switch (command)
+                {
+                    case "Product":
+                        result = controller.ProcessProductCommand(arguments);
+                        break;
+                    case "Service":
+                        result = controller.ProcessServiceCommand(arguments);
+                        break;
+                    case "Checkout":
+                        result = controller.ProcessCheckoutCommand(arguments);
+                        break;
+                    case "Info":
+                        result = controller.ProcessInfoCommand(arguments);
+                        break;
+                    case "End":
+                        result = controller.ProcessEndCommand();
+                        this.isRunning = false;
+                        break;
+                    default:
+                        result = "Invalid command";
+                        break;
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
